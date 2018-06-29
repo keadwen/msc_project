f21b48ac023a10c410f7aec68c81f266c47adfbd
Jakub Mlynarczyk <jkbmlynarczyk@gmail.com>
2018-06-29 13:44:07 +1000

    [msc] Implemented energy deduction for TX/RX

--- core/network.go
+++ core/network.go
@@ -2,7 +2,16 @@ package core
 
 import (
 	"fmt"
+	"math"
 	"sync"
+)
+
+const (
+	// Energy values measured in [J/byte].
+	E_TX = 40e-9      // Transmission
+	E_RX = 4e-9       // Receiving
+	E_MP = 0.0104e-12 // Multipath fading
+	E_FS = 80e-12     // Line of sight free space channel
 )
 
 type Network struct {
@@ -41,7 +50,9 @@ func (net *Network) Simulate() {
 			go func(n *Node) {
 				defer wg.Done()
 				// Simply send the message to Base Station.
-				n.Transmit(n.X, net.BaseStation)
+				if err := n.Transmit(int64(n.X), net.BaseStation); err != nil {
+					fmt.Println(err)
+				}
 			}(n.(*Node))
 			return true
 		})
@@ -68,17 +79,48 @@ type Node struct {
 	Energy float64
 
 	// Coordinates.
-	X int64
-	Y int64
+	X float64
+	Y float64
 }
 
 func (n *Node) Transmit(msg int64, dst *Node) error {
+	// Deduct cost of transmission.
+	var cost float64
+	if d := n.distance(dst); d > math.Sqrt(E_FS/E_MP) {
+		cost = E_TX + math.Pow(E_MP, 4)
+	} else {
+		cost = E_TX + math.Pow(E_MP, 2)
+	}
+	if err := n.consume(cost * float64(msg)); err != nil {
+		return err
+	}
+
 	fmt.Printf("node <%d> sends to node <%d>\n", n.ID, dst.ID)
-	dst.Receive(msg)
 	return nil
 }
 
 func (n *Node) Receive(msg int64) error {
+	// Deduct cost of receiving.
+	if err := n.consume(E_TX * float64(msg)); err != nil {
+		return err
+	}
+
 	fmt.Printf("node <%d> receive message <%d>\n", n.ID, msg)
+	return nil
+}
+
+func (n *Node) distance(dst *Node) float64 {
+	x := math.Abs(n.X - dst.X)
+	y := math.Abs(n.Y - dst.Y)
+	return math.Hypot(x, y)
+}
+
+func (n *Node) consume(e float64) error {
+	if n.Energy-e < 0 {
+		n.Ready = false
+		n.Energy = 0
+		return fmt.Errorf("node <%d> no more energy!", n.ID)
+	}
+	n.Energy -= e
 	return nil
 }
--- main.go
+++ main.go
@@ -11,13 +11,13 @@ func main() {
 		ID:     0,
 		Ready:  true,
 		Energy: 10000.0,
-		X:      0,
-		Y:      0,
+		X:      0.0,
+		Y:      0.0,
 	}
 
 	// Create 2 nodes.
-	net.AddNode(&core.Node{1, true, 10.0, 10, 0})
-	net.AddNode(&core.Node{2, true, 10.0, 20, 0})
+	net.AddNode(&core.Node{1, true, 100.0e-7, 10.0, 0.0})
+	net.AddNode(&core.Node{2, true, 100.0e-8, 20.0, 0.0})
 
 	// Run simulation for 10 rounds.
 	net.Simulate()
