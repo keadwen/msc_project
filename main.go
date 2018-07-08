91aceb71c7d302ed92104e6dc1f5c5fb2bb483d9
Jakub Mlynarczyk <jkbmlynarczyk@gmail.com>
2018-07-08 14:14:43 +1000

    [msc] Develop scenario creation in main

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/golang/protobuf/proto"
@@ -21,16 +26,19 @@ var (
 func main() {
 	flag.Parse()
 
-	var conf config.Config
+	var conf *config.Config
 	var err error
 	if *configFile == "" {
 		conf, err = createScenario()
+		if err != nil {
+			log.Fatalf("Failed to create scenario: %v", err)
+		}
 	} else {
 		data, err := ioutil.ReadFile(*configFile)
 		if err != nil {
 			log.Fatalf("Failed to open a file %q: %v", *configFile, err)
 		}
-		if err := proto.UnmarshalText(string(data), &conf); err != nil {
+		if err := proto.UnmarshalText(string(data), conf); err != nil {
 			log.Fatalf("Failed to unmarshal config proto %q: %v", *configFile, err)
 		}
 	}
@@ -75,6 +83,36 @@ func main() {
 }
 
 // createScenario provides an ability to run single scenario with CLI support.
-func createScenario() (config.Config, error) {
-	return config.Config{}, nil
+func createScenario() (*config.Config, error) {
+	reader := bufio.NewReader(os.Stdin)
+	// Get amount of nodes in scenario.
+	fmt.Print("Nodes in scenario [e.g. 5]: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, fmt.Errorf("failed to read input: %v", err)
+	}
+	nodes, err := strconv.Atoi(strings.TrimSpace(input))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse int %q: %v", input, err)
+	}
+
+	conf := &config.Config{}
+	// Create base station (without user interraction).
+	conf.Nodes = append(conf.Nodes, &config.Node{Id: 0, InitialEnergy: 1e100, Location: &config.Location{X: 0, Y: 0}})
+	fmt.Println("Created Base Station...")
+
+	// Create nodes with exact amount of energy, but random location [0, 1000].
+	for n := 1; n <= nodes; n++ {
+		conf.Nodes = append(conf.Nodes, &config.Node{
+			Id:            int64(n),
+			InitialEnergy: 1e-4,
+			Location: &config.Location{
+				X: rand.Float64() * 1000,
+				Y: rand.Float64() * 1000,
+			},
+		})
+		node := conf.Nodes[n]
+		fmt.Printf("Created node <%d> (E: %f[J] X: %2.f, Y: %2.f)...\n", n, node.GetInitialEnergy(), node.GetLocation().GetX(), node.GetLocation().GetY())
+	}
+	return conf, nil
 }
